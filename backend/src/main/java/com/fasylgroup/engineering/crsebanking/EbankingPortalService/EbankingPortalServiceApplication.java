5759636096d225ae71c91fab17cc18c114ca11e6
etornam anyidoho <etornamanyidoho@outlook.com>
2023-08-05 00:12:52 +0000

    project init

---
+++ backend/src/main/java/com/fasylgroup/engineering/crsebanking/EbankingPortalService/EbankingPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fasylgroup.engineering.crsebanking.EbankingPortalService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingPortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingPortalServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fasylgroup/engineering/crsebanking/EbankingPortalService/EbankingPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasylgroup.engineering.crsebanking.EbankingPortalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingPortalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
